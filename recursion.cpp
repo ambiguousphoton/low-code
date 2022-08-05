c04eeca6a49667c85b0a463945c1799ba5247f0a
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-05 23:59:47 +0530

    Add files via upload

---
+++ recursion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int temp = 1;
+
+void recursion(){
+    if (x==0){
+        return 
+    }
+    int y = x-1
+    int x = x * y
+    recursion();
+}
+
+int main(){
+    cout<<"input :: "
+    cin>>x;
+    
+    recursion();
+    return 0;
+}
