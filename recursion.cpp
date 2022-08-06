0bce397749cdc7c603d05e6c8d272d85b4a7ebb7
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-08-06 22:34:58 +0530

    Add files via upload

--- recursion.cpp
+++ recursion.cpp
@@ -1,21 +1,41 @@
+// #include <stdio.h>
 #include <iostream>
+// #include <thread>
 using namespace std;
-
 int temp = 1;
+int count = 0;
 
-void recursion(){
-    if (x==0){
-        return 
+int recursion(int x, int check)
+{
+    int factorial = x;
+    if (check == 0)
+    {
+        printf("total loops done :: %d ", count);
+
+        return factorial;
     }
-    int y = x-1
-    int x = x * y
-    recursion();
+    count++;
+    int y = check - 1;
+    check--;
+    if (y != 0)
+    {
+        x = x * y;
+    }
+    cout << y << " ";
+    recursion(x, check);
 }
 
-int main(){
-    cout<<"input :: "
-    cin>>x;
-    
-    recursion();
+int main()
+{
+    // using namespace std::this_thread;
+    // using namespace std::chrono;
+    int x;
+    cout << "input :: ";
+    cin >> x;
+    int check = x;
+    int factorial = recursion(x, check);
+    cout << "factorial of " << x << " :: " << factorial;
+    _sleep(5000);
+    // sleep_for(nanoseconds(5000000000));
     return 0;
 }
