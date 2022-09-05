988bd3a051365e231250e10fb3b505cee71558c8
vectorperfect <108605954+ambiguousphoton@users.noreply.github.com>
2022-09-05 20:51:10 +0530

    Update Planets.cpp

--- Planets.cpp
+++ Planets.cpp
@@ -6,6 +6,8 @@ float mass; // × 10^24 killograms
 float diy; // earth days in year of planet
 }
 int main(){
+struct planet earth;
+earth.no =4;
 struct planet venus; 
 venus.no = 1;
 venus.mass = 4.867;
